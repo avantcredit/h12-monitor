3efe346f4379b92608342b200c87bc6c62379f14
Gabe Saravia <gabe.saravia@avantcredit.com>
2014-08-27 16:13:13 -0500

    modify runner file to accept env vars

--- h12-monitor.rb
+++ h12-monitor.rb
@@ -1,19 +1,21 @@
 #!/usr/bin/env ruby
-
 require 'rubygems'
 require 'bundler'
-
 if defined?(Bundler)
   Bundler.require
 end
-
 require 'daemons'
 require File.expand_path('../lib/h12_monitor', __FILE__)
 
 # You can get your API key from Heroku's My Account page
-API_KEY = '--MY_API_KEY--'
-APP_NAME = '--MY_APP_NAME--'
+APP_NAME = ENV['H12_MONITOR_HEROKU_APP_NAME']
+API_KEY  = ENV['H12_MONITOR_HEROKU_APP_API_KEY']
 
-Daemons.run_proc('h12-monitor.rb') do
-  H12Monitor.new(APP_NAME, API_KEY).monitor
+if (APP_NAME && API_KEY)
+  Daemons.run_proc('h12-monitor.rb') do
+    H12Monitor.new(APP_NAME, API_KEY).monitor
+  end
+else
+  puts "YOU DONT HAVE H12 MONITOR VARS SET IN YOUR ENVIRONMENT"
+  puts "H12 Monitor will not run."
 end
